5ca4403abd51d2231e3acc8746d1b416becb41b5
KSYong <ericyong95@naver.com>
2023-01-07 14:17:01 +0900

    Initial Commit

---
+++ MoveYourBody/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MoveYourBody
+//
+//  Created by 권승용 on 2023/01/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoveYourBody/MoveYourBodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveYourBodyApp.swift
+//  MoveYourBody
+//
+//  Created by 권승용 on 2023/01/07.
+//
+
+import SwiftUI
+
+@main
+struct MoveYourBodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
