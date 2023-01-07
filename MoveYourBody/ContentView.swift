87f2bf093d38b00578d5e93c449e69b591e7cecd
Na Young Jung <nang518@icloud.com>
2023-01-07 15:48:13 +0900

    [test]: 커밋 테스트

--- MoveYourBody/ContentView.swift
+++ MoveYourBody/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("안녕하세요!")
         }
         .padding()
     }
