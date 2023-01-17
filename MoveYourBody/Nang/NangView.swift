b2d122987b24a7b2ddadf21fb9b9633d097327b9
Na Young Jung <nang518@icloud.com>
2023-01-18 04:26:26 +0900

    [feat]: 낭 알람설정 뷰 UI 작성

--- MoveYourBody/Nang/NangView.swift
+++ MoveYourBody/Nang/NangView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct NangView: View {
+
+    @State private var showSheet = false
+    @State private var selectedDate = Date()
+
+    //UI 예시
+    @State private var setAlarm1 = false
+
     var body: some View {
-        Text("안녕하세요~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        ZStack {
+            
+            VStack(spacing: 70) {
+                settingBar()
+            }
+            .environment(\.colorScheme, .dark)
+        }
+        
+    }
+
+    func settingBar() -> some View {
+        NavigationView {
+            VStack {
+                List {
+                    Toggle(isOn: $setAlarm1) {
+                        Text("아침 운동")
+                    }
+                }
+            }
+            .navigationBarTitle("운동 알림")
+            .navigationBarItems(leading: EditButton())
+            .navigationBarItems(trailing: Button {
+                self.showSheet = true
+            } label: {
+                Image(systemName: "plus")
+            }
+                .sheet(isPresented: self.$showSheet) {
+                    plusAlarm()
+                        .preferredColorScheme(.dark)
+                })
+        }
+    }
+
+    func plusAlarm() -> some View {
+        NavigationView {
+            
+                VStack {
+                    DatePicker("",selection: $selectedDate, displayedComponents: .hourAndMinute)
+                        .datePickerStyle(.wheel)
+                }
+                .navigationBarTitle("새로운 알람", displayMode: .inline)
+                .navigationBarItems(leading: Button(action: {
+                        self.showSheet = false}) {Text("취소")},
+                    trailing: Button(action: {
+                        print("저장")}) {Text("저장")})
+        }
     }
 }
 
