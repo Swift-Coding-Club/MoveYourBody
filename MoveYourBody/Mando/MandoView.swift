3cb4ce566962fa8e85345ca31c3300246a77c993
thisisthewa2 <thisisthewa2@gmail.com>
2023-01-18 20:28:28 +0900

    [feat] 만도 운동설정 뷰 ui 작성

--- MoveYourBody/Mando/MandoView.swift
+++ MoveYourBody/Mando/MandoView.swift
@@ -9,7 +9,130 @@ import SwiftUI
 
 struct MandoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment:.top) {
+            Color("background")
+                .ignoresSafeArea()
+            VStack(spacing:40){
+                HStack(){
+                        navHomeButton()
+                        mainTitle()
+                    }
+                    questionFirst()
+                    HStack{
+                        Button("서서 운동"){}
+                            .buttonStyle(OutlineButton())
+                        Button("앉아서 운동"){}
+                            .buttonStyle(OutlineButton())
+                    }
+                    questionSecond()
+                    VStack{
+                        HStack{
+                            Button("플랭크"){}
+                                .buttonStyle(OutlineButton())
+                            Button("허리 굽히기"){}
+                                .buttonStyle(OutlineButton())
+                            Button("런지"){}
+                                .buttonStyle(OutlineButton())
+                        }
+                        HStack{
+                            Button("무릎 굽히기"){}
+                                .buttonStyle(OutlineButton())
+                            Button("스쿼트"){}
+                                .buttonStyle(OutlineButton())
+                            Button("다리 찢기"){}
+                                .buttonStyle(OutlineButton())
+                        }
+                    }
+                
+                questionThird()
+                VStack{
+                    HStack{
+                        Button("3분 이하"){}
+                            .buttonStyle(OutlineButton())
+                        Button("5분"){}
+                            .buttonStyle(OutlineButton())
+                        Button("5분 이상"){}
+                            .buttonStyle(OutlineButton())
+                    }}
+                }
+            }
+        
+    }
+    
+
+    @ViewBuilder
+    func mainTitle() -> some View {
+        VStack {
+            Text("운동 설정")
+                .font(.system(size: 35, weight: .heavy))
+                .multilineTextAlignment(.center)
+        }
+        .foregroundColor(Color("titleForeground"))
+    }
+    
+    @ViewBuilder
+    func questionSecond() -> some View {
+        VStack {
+            Text("하고 싶지 않은 동작이 있나요?")
+                .font(.system(size: 25, weight: .heavy))
+                .multilineTextAlignment(.leading)
+        }
+        .foregroundColor(Color("titleForeground"))
+    }
+    
+    @ViewBuilder
+    func questionFirst() -> some View {
+        VStack {
+            Text("선호하는 운동 방식이 있나요?")
+                .font(.system(size: 25, weight: .heavy))
+                .multilineTextAlignment(.leading)
+        }
+        .foregroundColor(Color("titleForeground"))
+    }
+    
+    @ViewBuilder
+    func questionThird() -> some View {
+        VStack {
+            Text("1회에 얼마나 운동하고 싶나요?")
+                .font(.system(size: 25, weight: .heavy))
+                .multilineTextAlignment(.leading)
+        }
+        .foregroundColor(Color("titleForeground"))
+    }
+    
+    
+    @ViewBuilder
+    func navHomeButton() -> some View {
+        Button {
+            
+        } label: {
+            Image("back-button")
+        }
+    }
+    
+    @ViewBuilder
+    func optionButton() -> some View {
+        Button("스쿼트") {}
+            .foregroundColor(.black)
+            .padding()
+            .background(.white)
+            .cornerRadius(8)
+    }
+    
+    struct OutlineButton: ButtonStyle {
+        func makeBody(configuration: Configuration) -> some View {
+            
+            configuration
+                .label
+                .foregroundColor(configuration.isPressed ? .green : .white)
+                .padding()
+                .background(
+                    RoundedRectangle(
+                        cornerRadius: 8,
+                        style: .continuous
+                    ).stroke(configuration.isPressed ? .green : .white)
+            )
+        }
     }
 }
 
