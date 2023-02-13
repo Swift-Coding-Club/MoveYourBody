d7b2c258d98772894c385b2cf08a855f0edd2077
thisisthewa2 <thisisthewa2@gmail.com>
2023-02-13 14:13:13 +0900

    [feat] : 만도 @AppStorage로 설정 사항 저장

--- MoveYourBody/View/WorkoutSettingsView.swift
+++ MoveYourBody/View/WorkoutSettingsView.swift
@@ -6,11 +6,20 @@
 //
 
 import SwiftUI
+import Foundation
 
 
 struct WorkoutSettingsView: View {
-    @State private var buttonBackColor: Color = Color("neongreen")
-    @StateObject var vm = ContentViewModel()
+    @AppStorage("toggle1") var didTap1: Bool = DefaultSettings.didTap1
+    @AppStorage("toggle2") var didTap2: Bool = false
+    @AppStorage("toggle3") var didTap3: Bool = false
+    @AppStorage("toggle4") var didTap4: Bool = false
+    @AppStorage("toggle5") var didTap5: Bool = false
+    
+    @AppStorage("toggle6") var didTap6: Bool = false
+    @AppStorage("toggle7") var didTap7: Bool = false
+    
+//  let squat = Foundation.UserDefaults.standard.bool(forKey: "isClicked")
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var btnBack: some View{
@@ -20,36 +29,108 @@ struct WorkoutSettingsView: View {
             }.navigationBarBackButtonHidden(true)
         }
     }
-    
-    var body: some View {
+    var squat = Foundation.UserDefaults.standard.bool(forKey: "isClicked")
+    var body: some View{
         NavigationView{
-        ZStack{
-            Color("background")
-                .ignoresSafeArea()
-            VStack {
-                ForEach(vm.filteredItems){
-                    item in HStack{
-                        Button(action:{
-                            vm.toggleCheck(item: item)
-                        }){
-                            Text(item.title)
-                                .font(.headline)
+            ZStack{
+                Color("background")
+                    .ignoresSafeArea()
+                VStack (spacing:80){
+                    VStack(spacing: 20){
+                        Text("하고 싶지 않은 동작이 있나요?")
+                        HStack{
+                            Button(action:{
+                                self.didTap1.toggle()
+                            }){
+                                Text("스쿼트")
+                                    .padding()
+                            }
+                            .background(didTap1 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                            
+                            Button(action:{
+                                self.didTap2.toggle()
+                            }){
+                                Text("플랭크")
+                                    .padding()
+                            }
+                            .background(didTap2 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                            
+                            Button(action:{
+                                self.didTap3.toggle()
+                            }){
+                                Text("런지")
+                                    .padding()
+                            }
+                            .background(didTap3 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                            
                         }
-                        .foregroundColor(Color.black)
-                        .font(.system(size:14).bold())
-                        .padding(15)
-                        .background(vm.contains(item) ? Color.green: Color.red)
+                        HStack{
+                            Button(action:{
+                                self.didTap4.toggle()
+                            }){
+                                Text("허리 굽히기")
+                                    .padding()
+                            }
+                            .background(didTap4 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                            
+                            Button(action:{
+                                self.didTap5.toggle()
+                            }){
+                                Text("무릎 굽히기")
+                                    .padding()
+                            }
+                            .background(didTap5 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                        }
+                    }
+                    
+                    VStack{
+                        Text("1회 운동 시간은 얼만큼이 좋은가요?")
+                        HStack{
+                            Button(action:{
+                                self.didTap6.toggle()
+                            }){
+                                Text("3분 이하")
+                                    .padding()
+                            }
+                            .background(didTap6 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                            
+                            Button(action:{
+                                self.didTap7.toggle()
+                            }){
+                                Text("5분 이하")
+                                    .padding()
+                            }
+                            .background(didTap7 ? Color.red : Color.green)
+                            .cornerRadius(18)
+                        }
                         
                     }
+                    
+                }
+                .foregroundColor(Color.white)
+                                .navigationBarItems(leading: btnBack)
+                                
+            }
+        }.navigationBarBackButtonHidden()
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Title")
+                        .font(.largeTitle.bold())
+                        .accessibilityAddTraits(.isHeader)
                 }
             }
-                .navigationTitle("Hi")
-                .foregroundColor(Color.white)
-                .navigationBarItems(leading: btnBack, trailing: saveButton())
-            }
-        }.navigationBarBackButtonHidden(true)
+        
     }
 }
+    
+
 
 @ViewBuilder
 func saveButton() -> some View {
@@ -62,127 +143,27 @@ func saveButton() -> some View {
             .multilineTextAlignment(.center)
             .padding()
             .overlay(
-                RoundedRectangle(cornerRadius: 15)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
+                RoundedRectangle(cornerRadius: 15)       .stroke(OutlineButtonStyle.neongreen, lineWidth: 3))
         
     }
 }
 
-@ViewBuilder
-func optionFirstButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("3분이하")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
 
-@ViewBuilder
-func mnFirstButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("스쿼트")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
 
-@ViewBuilder
-func mnSecondButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("플랭크")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
 
-@ViewBuilder
-func mnThirdButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("런지")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
-
-@ViewBuilder
-func mnFourthButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("허리 굽히기")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
-
-@ViewBuilder
-func mnFifthButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("무릎 굽히기")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
-
-@ViewBuilder
-func optionSecondButton() -> some View {
-    Button {
-        
-    } label: {
-        Text("5분이하")
-            .foregroundColor(Color.white)
-            .font(.system(size:14).bold())
-            .padding()
-            .overlay(
-                RoundedRectangle(cornerRadius: 20)       .stroke(OutlineButtonSytle.neongreen, lineWidth: 3))
-        
-    }
-}
-
-struct OutlineButtonSytle: ButtonStyle{
+struct OutlineButtonStyle: ButtonStyle{
     static let neongreen=Color("neon-green")
-    func makeBody(configuration: Configuration) -> some View {
+    func makeBody(configuration: Self.Configuration) -> some View {
         configuration.label
             .foregroundColor(Color.white)
             .font(.system(size:14).bold())
             .padding()
             .overlay(
                 RoundedRectangle(cornerRadius: 20)
-                    .stroke(OutlineButtonSytle.neongreen, lineWidth: 3)
+                    .stroke(OutlineButtonStyle.neongreen, lineWidth: 3)
             )
+        
+            
     }
 }
 
